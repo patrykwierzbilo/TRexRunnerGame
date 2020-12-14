cd7e1824ea93fa103024be99c7d8af438f22dea1
patrykwierzbilo1 <patryk.wierzbilo@gmail.com>
2020-12-14 19:54:36 +0100

    Implementation of the class diagram

---
+++ Game/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRexRunnerGame.Game
+{
+    class Game
+    {
+        private TRexRunnerGame.Game.Score.IMediator mediator;
+        private IStrategy strategy;
+        private Graphics.IGraphicsComponent graphics;
+        private Input.IInputComponent input;
+    }
+}
