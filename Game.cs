9436aac2f7cf8f0c6aa30e26b2a47a1cb6607d92
patrykwierzbilo1 <patryk.wierzbilo@gmail.com>
2021-01-06 10:00:38 +0100

    Fix initialize components and add game reset

--- Game.cs
+++ Game.cs
@@ -11,14 +11,26 @@ namespace TRexRunnerGame
 {
     class Game : Form
     {
+        private IContainer components = null;
         List<Object> entities = new List<object>();
+        Timer gameTimer;
+        bool jumping = false;
+        int jumpSpeed = 10;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
         public Game()
         {
             InitComponents();
+            GameReset();
         }
 
         private void InitComponents()
         {
+            this.SuspendLayout();//prob out
             Entity ground = new Entity();
             ground.graphics = new GroundGraphics();
             AddControl(ground);
@@ -34,11 +46,63 @@ namespace TRexRunnerGame
             Entity score = new Entity();
             score.graphics = new ScoreGraphics();
             AddControl(score);
+            //timer
+
+            //((ISupportInitialize)(ground.graphics.GetControl())).BeginInit();
+            //((ISupportInitialize)(bc.graphics.GetControl())).BeginInit();
+            //((ISupportInitialize)(lc.graphics.GetControl())).BeginInit();
+            //((ISupportInitialize)(trex.graphics.GetControl())).BeginInit();
+            gameTimer = new Timer();
+            gameTimer.Interval = 20;
+            gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            
+            this.SuspendLayout();//need check
+
             this.ResumeLayout(false);
             this.PerformLayout();
             Text = "TRex Game";
             ClientSize = new Size(800, 450);
             CenterToScreen();
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            //scoreText.Text = "Score: " + score;
+            //trex.Image = Properties.Resources.running;
+            isGameOver = false;
+            //trex.Top = 272;//Ground.Top - trex.Height;
+            
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Name.EndsWith("Cactus"))
+                {
+                    position = this.ClientSize.Width + rand.Next(300, 600) + (x.Width * 10);
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+
         }
         void AddControl(Entity entity)
         {
--- ScoreGraphics.cs
+++ ScoreGraphics.cs
@@ -17,7 +17,7 @@ namespace TRexRunnerGame
             scoreText.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, 
                                                     System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             scoreText.Location = new System.Drawing.Point(45, 31);
-            scoreText.Name = "txtScore";
+            scoreText.Name = "scoreText";
             scoreText.Size = new System.Drawing.Size(135, 33);
             scoreText.TabIndex = 5;
             scoreText.Text = "Score: 0";
